22395598bda75e1f211bbdd0edd71d07641b4ccc
username <Jzazzi56@gmail.com>
2024-09-30 09:55:05 -0700

    made home page with links to categories

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,64 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./styles.scss";
+import images from "./images";
 
 /*
 array of food by category, category leads to a page with all the food in that category
 */
 
 const Home = () => {
-  const [categories, setCategories] = useState([]);
-
   return (
     <div className="home-main-container">
       <div className="home-title-container">
         <h1>Joey&apos;s Cookbook</h1>
       </div>
       <div className="home-cat-container">
-        <h2>Categories</h2>
-        <div className="home-categories">
-          {/* map through categories */}
-          {categories.map((cat, index) => (
-            <div key={index}>
-              <h3>{cat.name}</h3>
-              <img>{cat.image}</img>
-            </div>
-          ))}
+        <div className="cat-links-container">
+          <div className="cat-link-container">
+            <Link
+              to="/category/1"
+              className="cat-link"
+              style={{
+                backgroundImage: `url(${images.breakfast})`,
+                backgroundSize: "cover",
+              }}
+            >
+              Breakfast
+            </Link>
+            <Link
+              to="/category/2"
+              className="cat-link"
+              style={{
+                backgroundImage: `url(${images.lunch})`,
+                backgroundSize: "cover",
+              }}
+            >
+              Lunch
+            </Link>
+            <Link
+              to="/category/3"
+              className="cat-link"
+              style={{
+                backgroundImage: `url(${images.dinner})`,
+                backgroundSize: "cover",
+              }}
+            >
+              Dinner
+            </Link>{" "}
+            <Link
+              to="/category/4"
+              className="cat-link"
+              style={{
+                backgroundImage: `url(${images.dessert})`,
+                backgroundSize: "cover",
+              }}
+            >
+              Dessert
+            </Link>
+          </div>
         </div>
+        <div className="home-categories"></div>
       </div>
     </div>
   );

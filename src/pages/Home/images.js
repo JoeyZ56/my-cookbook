1af55e5ccc84b1b43646f56fdf3d8572c8ee5f9c
username <Jzazzi56@gmail.com>
2024-09-30 09:55:45 -0700

    added images file for easy export

---
+++ src/pages/Home/images.js
@@ -0,0 +1,7 @@
+import breakfast from "../../assets/home-page-images/breakfast.png";
+import lunch from "../../assets/home-page-images/lunch.png";
+import dinner from "../../assets/home-page-images/dinner.png";
+import dessert from "../../assets/home-page-images/dessert.png";
+
+const images = { breakfast, lunch, dessert, dinner };
+export default images;

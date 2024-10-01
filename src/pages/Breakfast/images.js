4cbb678fbbd69eaae75ef6955738b58b8158f452
username <Jzazzi56@gmail.com>
2024-10-01 14:53:07 -0700

    adding an index page for easy accessing images for breakfast items

---
+++ src/pages/Breakfast/images.js
@@ -0,0 +1,11 @@
+import eggs from "../../assets/breakfast/eggs.png";
+import pancakes from "../../assets/breakfast/pancakes.png";
+import quiche from "../../assets/breakfast/quiche.png";
+
+const images = {
+  eggs,
+  pancakes,
+  quiche,
+};
+
+export default images;

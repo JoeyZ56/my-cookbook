2474e07a16e26ce84ceacc4e442878d7fcb44c68
username <Jzazzi56@gmail.com>
2024-10-03 15:15:22 -0700

    index of images for lunches for easy importing

---
+++ src/pages/Lunch/images.js
@@ -0,0 +1,7 @@
+import sandwiches from "../../assets/lunch/sandwiches.png";
+import wraps from "../../assets/lunch/wraps.png";
+import salads from "../../assets/lunch/salads.png";
+
+const images = { salads, sandwiches, wraps };
+
+export default images;

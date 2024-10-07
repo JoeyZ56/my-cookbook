0b3fd3bdf30713b61936965283726fb0a26fba75
username <Jzazzi56@gmail.com>
2024-10-07 15:45:32 -0700

    image index for dinner category

---
+++ src/pages/Dinner/images.js
@@ -0,0 +1,9 @@
+import American from "../../assets/dinner/american.png";
+import Mexican from "../../assets/dinner/mexican.png";
+import Asain from "../../assets/dinner/asain.png";
+import MiddleEastern from "../../assets/dinner/middle-eastern.png";
+import Italian from "../../assets/dinner/italian.png";
+
+const images = { American, Mexican, Asain, MiddleEastern, Italian };
+
+export default images;

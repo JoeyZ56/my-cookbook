8f8a86a7849ea7c554136365c2f89238e59fe4e7
username <Jzazzi56@gmail.com>
2024-10-07 15:43:41 -0700

    added dinner category

---
+++ src/pages/Dinner/Dinner.jsx
@@ -0,0 +1,68 @@
+import "./styles.scss";
+import { Link } from "react-router-dom";
+import images from "./images";
+
+const Dinner = () => {
+  const handleBackBtn = () => {
+    window.history.back();
+  };
+
+  return (
+    <>
+      <button onClick={handleBackBtn} className="dinner-back-btn">
+        back
+      </button>
+      <div className="dinner-main-container">
+        <div className="dinner-links-container">
+          <Link
+            className="link"
+            style={{
+              backgroundImage: `url(${images.American})`,
+              backgroundSize: "cover",
+            }}
+          >
+            American Cuisines
+          </Link>
+          <Link
+            className="link"
+            style={{
+              backgroundImage: `url(${images.Mexican})`,
+              backgroundSize: "cover",
+            }}
+          >
+            Mexican Cuisines
+          </Link>
+          <Link
+            className="link"
+            style={{
+              backgroundImage: `url(${images.Mexican})`,
+              backgroundSize: "cover",
+            }}
+          >
+            Italian Cuisines
+          </Link>
+          <Link
+            className="link"
+            style={{
+              backgroundImage: `url(${images.Italian})`,
+              backgroundSize: "cover",
+            }}
+          >
+            Asain Cuisines
+          </Link>
+          <Link
+            className="link"
+            style={{
+              backgroundImage: `url(${images.MiddleEastern})`,
+              backgroundSize: "cover",
+            }}
+          >
+            Middle Eastern Cuisines
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dinner;

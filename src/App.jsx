2729db46cd3cadf471a05940bdcc5f1210dda241
username <Jzazzi56@gmail.com>
2024-10-11 14:30:38 -0700

    routes to category pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+//Pages
+import Home from "./pages/Home/Home";
+import Breakfast from "./pages/Breakfast/Breakfast";
+import Lunch from "./pages/Lunch/Lunch";
+import Dinner from "./pages/Dinner/Dinner";
+import Dessert from "./pages/Dessert/Dessert";
+import RecipeForm from "./pages/RecipeForm/RecipeForm";
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/breakfast" element={<Breakfast />} />
+        <Route path="/lunch" element={<Lunch />} />
+        <Route path="/dinner" element={<Dinner />} />
+        <Route path="/dessert" element={<Dessert />} />
+        <Route path="/recipe-from" element={<RecipeForm />} />
       </Routes>
     </Router>
   );

173d5a2e88df7c95abad0f54f2a9f807fe921c2d
username <Jzazzi56@gmail.com>
2025-01-16 14:11:08 -0800

    updated color design of menu

--- src/components/hamburgerMenu.jsx
+++ src/components/hamburgerMenu.jsx
@@ -7,6 +7,7 @@ import {
   List,
   ListItem,
   ListItemText,
+  Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
@@ -26,11 +27,23 @@ export default function HamburgerMenu() {
 
   return (
     <>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: "#FFC107" }}>
         <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              color: "#000",
+              fontWeight: "bold",
+              fontSize: "28px",
+            }} // FlexGrow makes it align left
+          >
+            DishBoard
+          </Typography>
           <IconButton
             edge="start"
-            color="inherit"
+            color="#795548"
             aria-label="menu"
             onClick={toggleDrawer(true)}
           >
@@ -39,16 +52,40 @@ export default function HamburgerMenu() {
         </Toolbar>
       </AppBar>
 
-      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+        sx={{
+          "& .MuiDrawer-paper": {
+            backgroundColor: "#FFF3E0", // Set the background color of the entire drawer
+          },
+        }}
+      >
         <List>
+          <ListItem button component={Link} to="/">
+            <ListItemText primary="Home" sx={{ color: "#000" }} />
+          </ListItem>
+          <ListItem button component={Link} to="/breakfast">
+            <ListItemText primary="Breakfast Recipes" sx={{ color: "#000" }} />
+          </ListItem>
+          <ListItem button component={Link} to="/lunch">
+            <ListItemText primary="Lunch Recipes" sx={{ color: "#000" }} />
+          </ListItem>
+          <ListItem button component={Link} to="/dinner">
+            <ListItemText primary="Dinner Recipes" sx={{ color: "#000" }} />
+          </ListItem>
+          <ListItem button component={Link} to="/dessert">
+            <ListItemText primary="Dessert Recipes" sx={{ color: "#000" }} />
+          </ListItem>
           <ListItem button component={Link} to="/recipe-form">
-            <ListItemText primary="Create Recipe" />
+            <ListItemText primary="Create Recipe" sx={{ color: "#000" }} />
           </ListItem>
           <ListItem button component={Link} to="/signup">
-            <ListItemText primary="Sign Up" />
+            <ListItemText primary="Sign Up" sx={{ color: "#000" }} />
           </ListItem>
           <ListItem button component={Link} to="/login">
-            <ListItemText primary="Log In" />
+            <ListItemText primary="Log In" sx={{ color: "#000" }} />
           </ListItem>
         </List>
       </Drawer>

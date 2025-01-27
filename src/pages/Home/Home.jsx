1c4b33772cf474e18cb6ebdc4e94d38313a3c101
username <Jzazzi56@gmail.com>
2025-01-27 15:18:56 -0800

    update to structure of home pafe

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,70 +1,60 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./styles.scss";
+import { Box, Grid, Typography, Button, CircularProgress } from "@mui/material";
 import images from "./images";
+import HamburgerMenu from "../../components/hamburgerMenu";
 
 /*
 array of food by category, category leads to a page with all the food in that category
 */
 
 const Home = () => {
+  const [recipes, setRecipes] = useState();
+  const [likedRecipes, setLikedRecipes] = useState();
+  const [categories, setCategories] = useState();
+  const [search, setSearch] = useState();
+  const [searchResults, setSearchResults] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  //handle search
+  const handleSearch = () => {
+    setSearchResults(recipes.filter((recipe) => recipe.name === search));
+  };
+
+  //handle category
+  const handleFilterCategory = (category) => {
+    setSearchResults(recipes.filter((recipe) => recipe.catefory === category));
+  };
+
+  //get all recipes
+  useEffect(() => {
+    const fetchRecipes = async () => {};
+  }, []);
   return (
-    <div className="home-main-container">
-      <div className="home-title-container">
-        <h1>Joey&apos;s Cookbook</h1>
-      </div>
-      <div className="home-cat-container">
-        <div className="cat-links-container">
-          <div className="cat-link-container">
-            <Link
-              to="/breakfast"
-              className="cat-link"
-              style={{
-                backgroundImage: `url(${images.breakfast})`,
-                backgroundSize: "cover",
-              }}
-            >
-              Breakfast
-            </Link>
-            <Link
-              to="/lunch"
-              className="cat-link"
-              style={{
-                backgroundImage: `url(${images.lunch})`,
-                backgroundSize: "cover",
-              }}
-            >
-              Lunch
-            </Link>
-            <Link
-              to="/dinner"
-              className="cat-link"
-              style={{
-                backgroundImage: `url(${images.dinner})`,
-                backgroundSize: "cover",
-              }}
-            >
-              Dinner
-            </Link>{" "}
-            <Link
-              to="/dessert"
-              className="cat-link"
-              style={{
-                backgroundImage: `url(${images.dessert})`,
-                backgroundSize: "cover",
-              }}
-            >
-              Dessert
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className="home-recipe-form-container">
-        <Link to="/recipe-from" className="home-recipe-form-link">
-          Add A Recipe!
-        </Link>
-      </div>
-    </div>
+    <Box>
+      <HamburgerMenu />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#FFF3E0",
+          // width: "95%",
+        }}
+      >
+        <Typography
+          variant="h4"
+          textAlign="center"
+          gutterBottom
+          sx={{ marginTop: "3rem" }}
+        >
+          Recipes
+        </Typography>
+        <Box>{}</Box>
+      </Box>
+    </Box>
   );
 };
 
